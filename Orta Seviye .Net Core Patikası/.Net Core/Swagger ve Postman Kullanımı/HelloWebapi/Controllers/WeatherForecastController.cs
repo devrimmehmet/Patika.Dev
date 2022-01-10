3b055faf23b788440785cd728ad8fdeb27555da4
DevrimMehmet <devrimmehmet@gmail.com>
2022-01-11 01:12:06 +0300

    Swagger UI, Postman, Api Debug konularına ait ders notlarım(tekrar incelenmeli)

--- Orta Seviye .Net Core Patikası/.Net Core/Swagger ve Postman Kullanımı/HelloWebapi/Controllers/WeatherForecastController.cs
+++ Orta Seviye .Net Core Patikası/.Net Core/Swagger ve Postman Kullanımı/HelloWebapi/Controllers/WeatherForecastController.cs
@@ -35,5 +35,17 @@ namespace HelloWebapi.Controllers
             })
             .ToArray();
         }
+        [HttpGet("{id}")]
+        public ActionResult<WeatherForecast> GetById(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[0];
+        }
     }
 }
