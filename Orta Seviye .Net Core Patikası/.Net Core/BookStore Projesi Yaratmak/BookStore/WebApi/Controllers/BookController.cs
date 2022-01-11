b4def157dd259c5314eee17af528f2ab8e4730d9
DevrimMehmet <devrimmehmet@gmail.com>
2022-01-11 04:09:57 +0300

    Get Endpointlerinin yazılması

--- Orta Seviye .Net Core Patikası/.Net Core/BookStore Projesi Yaratmak/BookStore/WebApi/Controllers/BookController.cs
+++ Orta Seviye .Net Core Patikası/.Net Core/BookStore Projesi Yaratmak/BookStore/WebApi/Controllers/BookController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 namespace WebApi.Controllers
 {
     [ApiController]
@@ -22,6 +24,34 @@ namespace WebApi.Controllers
                 PageCount=250,
                 PublishDate=new System.DateTime(2010,05,23)
             },
+            new Book{
+                Id=3,
+                Title="Dune",
+                GenreId=2, // Science Fiction
+                PageCount=540,
+                PublishDate=new System.DateTime(2002,12,21)
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x=> x.Id).ToList<Book>();
+            return bookList;
         }
+        
+        [HttpGet("{id}")] // idyi roottan alıyoruz.
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book=> book.Id==id).SingleOrDefault();
+            return book;
+        }
+        
+       // [HttpGet] // idyi fromquery alıyoruz.
+       // public Book Get ( [FromQuery] string id)
+       // {
+       //     var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+       //     return book;
+       // }
     }
 }
