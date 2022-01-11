ba9c7084ec4f8c7c4c552268f93ab938c5689c63
DevrimMehmet <devrimmehmet@gmail.com>
2022-01-11 04:39:45 +0300

    http put

--- Orta Seviye .Net Core Patikası/.Net Core/BookStore Projesi Yaratmak/BookStore/WebApi/Controllers/BookController.cs
+++ Orta Seviye .Net Core Patikası/.Net Core/BookStore Projesi Yaratmak/BookStore/WebApi/Controllers/BookController.cs
@@ -53,5 +53,38 @@ namespace WebApi.Controllers
        //     var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
        //     return book;
        // }
+
+       
+       //Post
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x=> x.Title==newBook.Title);
+             //(book!=null)
+            if(book is not null)   
+                return BadRequest();
+            
+        
+            BookList.Add(newBook);
+            return Ok();
+            
+        }
+
+        //Put
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody]Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x=>x.Id==id);
+            if(book is null)
+            {
+                return BadRequest();
+            }else{
+                book.GenreId=updatedBook.GenreId!=default ? updatedBook.GenreId : book.GenreId;
+                book.PageCount=updatedBook.PageCount!=default ? updatedBook.PageCount : book.PageCount;
+                book.PublishDate=updatedBook.PublishDate!=default ? updatedBook.PublishDate : book.PublishDate;
+                book.Title=updatedBook.Title != default ? updatedBook.Title:book.Title;
+                return Ok();
+            }
+        }
     }
 }
